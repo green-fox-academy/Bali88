c8e988822c8a4ce341cfa821cc3ce9784ea09a2f
Bali88 <balazsfekete19@gmail.com>
2020-03-09 19:21:02 +0100

    add week2/day1

---
+++ week02/day01/src/Matrix.java
@@ -0,0 +1,9 @@
+public class Matrix {
+    public static void main(String[] args) {
+
+        int[][] multi = new int[][];
+
+
+
+    }
+}
---
+++ week02/day01/src/SecondElements.java
@@ -0,0 +1,8 @@
+public class SecondElements {
+    public static void main(String[] args) {
+        int[] myList = {1, 2, 3, 6};
+        System.out.println("The second element is " + myList[2]);
+    }
+
+}
+
