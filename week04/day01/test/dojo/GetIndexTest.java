08b6ac7807957ba4fee9ad7abbc6e78c4a54e673
Balazs Fekete <balazsfekete19@gmail.com>
2020-08-03 11:23:19 +0200

    Add dojo1

---
+++ week04/day01/test/dojo/GetIndexTest.java
@@ -0,0 +1,22 @@
+package dojo;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+import org.junit.jupiter.api.Test;
+
+public class GetIndexTest {
+  
+//  @Test
+//  public int getIndex() {
+//    assertEquals(-1)
+//
+//  }
+
+  @Test
+  public int getIndexContains() {
+    assertEquals(-1, contains(list, v));
+
+  }
+}
+}
