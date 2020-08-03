a9bfc74f661e87890b8d233da2a6a955d7b11367
Balazs Fekete <balazsfekete19@gmail.com>
2020-08-03 11:46:31 +0200

    Add getIndex with tests

--- week04/day01/test/dojo/GetIndexTest.java
+++ week04/day01/test/dojo/GetIndexTest.java
@@ -6,7 +6,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 public class GetIndexTest {
-  
+
 //  @Test
 //  public int getIndex() {
 //    assertEquals(-1)
